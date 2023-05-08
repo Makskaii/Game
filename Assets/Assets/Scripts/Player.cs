34b313e5af1c565de2e9e7f38f0c053b419a2bbe
Makskaii <maksimkorz14@mail.ru>
2023-05-08 20:16:20 +0300

    nothing

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class Player : MonoBehaviour
+{
+    public bool IsAlive { get; private set; } = true;
+    public bool HasKey { get; private set; } = false;
+    
+    private PlayerMovement _movement;
+
+    private void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Start()
+    {
+        Enable();
+    }
+
+    public void Enable()
+    {
+        _movement.enabled = true;
+    }
+
+    public void Disable()
+    {
+        _movement.enabled = false;
+    }
+
+    public void Kill()
+    {
+        IsAlive = false;
+    }
+
+    public void PickUpKey()
+    {
+        HasKey = true;
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask _obstacleMask;
+    [SerializeField] private float _step;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Enemy")
+        {
+            GetComponent<Player>().Kill();
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            TryMove(Vector3.forward);
+            
+        }
+        
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            TryMove(Vector3.back);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        { 
+            TryMove(Vector3.right);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        { 
+            TryMove(Vector3.left);
+            
+        }
+        if (transform.position.y < -10f)
+        {
+            GetComponent<Player>().Kill();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    
+    private void TryMove(Vector3 direction)
+    {
+        var forwardRay = new Ray(transform.position, direction);
+
+        if (Physics.Raycast(forwardRay, _step, _obstacleMask))
+            return;
+
+        transform.forward = direction;
+        transform.Translate(direction * _step, Space.World);
+    }
+}
